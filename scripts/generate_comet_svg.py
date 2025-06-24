81be4710301e897d1737ab9629b7944cac765cef
Vishal Singh <62753499+vsingh10@users.noreply.github.com>
2025-06-25 01:24:34 +0530

    comet_trail

---
+++ scripts/generate_comet_svg.py
@@ -0,0 +1,35 @@
+import svgwrite
+import math
+
+def generate_comet_svg(filename="github-contribution-grid-comet.svg"):
+    dwg = svgwrite.Drawing(filename, profile='tiny', size=("800px", "200px"))
+    comet_color = "#00fff7"
+    trail_length = 8
+
+    for frame in range(50):
+        frame_group = dwg.g(id=f'frame-{frame}', visibility="hidden")
+        for i in range(trail_length):
+            offset = frame - i
+            if 0 <= offset < 40:
+                x = 20 * offset
+                y = 100 + int(30 * math.sin(offset / 4.0))
+                opacity = 1.0 - (i / trail_length)
+                frame_group.add(dwg.circle(center=(x, y), r=6,
+                                           fill=comet_color, opacity=opacity))
+        dwg.add(frame_group)
+
+    animate_script = """
+    <![CDATA[
+        let frame = 0;
+        setInterval(() => {
+            for (let i = 0; i < 50; i++) {
+                document.getElementById('frame-' + i).setAttribute('visibility', i === frame ? 'visible' : 'hidden');
+            }
+            frame = (frame + 1) % 50;
+        }, 80);
+    ]]>
+    """
+    dwg.add(dwg.script(content=animate_script))
+    dwg.save()
+
+generate_comet_svg()
